e94ce7267bbd3336beafde3a370555e2b1e9011c
utkarsh <utkarshsingh.vdoit@gmail.com>
2024-11-27 00:57:03 +0530

    fix:next.config for removing caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)", // Apply to all API routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store", // Prevent caching for dynamic API responses
+          },
+        ],
+      },
+    ];
+  },
+};
 export default nextConfig;
